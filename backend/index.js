7fe5ccbb825d342876c85e660af149abf286f234
Paul Bremer <info@paulbremer.nl>
2019-02-04 10:37:41 +0100

    feat: updated mongoose added env conf

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mongoose.connect(`mongodb://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASS}.mlab.com:42459/rewind-db`, { useNewUrlParser: true })
+const mongoDB = `mongodb://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASS}@ds042459.mlab.com:42459/rewind-db?authSource=rewind-db&w=1`;
+
+// Exit application on error
+mongoose.connection.on('error', err => {
+    console.log(`MongoDB connection error: ${err}`);
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB is connected');
+});
+
+const connection = mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 const server = new GraphQLServer({ schema });
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log('Server is running on localhost:4000'));
