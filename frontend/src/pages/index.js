4bd59199d0a10775c01af2f7a17bbe7161c91315
Paul Bremer <info@paulbremer.nl>
2019-03-08 15:40:55 +0100

    Fixed graphql query

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -14,7 +14,6 @@ const Title = styled.h1`
 
 const IndexPage = ({ data }) => {
     const talks = data;
-
     console.log(talks);
 
     return (
@@ -25,23 +24,40 @@ const IndexPage = ({ data }) => {
             <p>Now go build something great.</p>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
                 <Image />
-                
 
-                { talks.rwconf.talks.map(item => (
-                    <p>{item.id}</p>
+                {talks.rwconf.lineUps[0].talks.map(talk => (
+                    <div key={talk.id}>
+                        <h3>{talk.title}</h3>
+                        <h4>{talk.speaker}</h4>
+                        <p>{talk.description}</p>
+                    </div>
                 ))}
-
             </div>
             <Link to="/page-2/">Go to page 2</Link>
         </Layout>
     );
 };
 
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+
 export const query = graphql`
-    {
+    query getCurrentLineUp {
         rwconf {
-            talks {
+            lineUps(where: { year: 2019, month: 2 }) {
                 id
+                year
+                month
+                talks {
+                    id
+                    speaker
+                    title
+                    youtubeId
+                    description
+                    conference
+                    tags
+                }
             }
         }
     }
