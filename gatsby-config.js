247eda105f9c0fc0cc43aaa1eca964fad1900218
Paul Bremer <info@paulbremer.nl>
2019-04-07 22:23:46 +0200

    chore: edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteMetadata: {
         title: `Rewind Conf`,
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`
+        author: `@paulbremer`
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
